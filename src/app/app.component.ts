ff71c95a9c824225c623dcc84a35ee991b0f2a7b
Gunnar Sigstad <gunnar.sigstad@storebrand.no>
2019-12-17 15:22:01 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ChristmasBrothersSister';
+
+  public question: number;
+  public errorMessage: string;
+  public opened: boolean;
+  public error: boolean;
+  public finished: boolean;
+
+  ngOnInit() {
+    this.question = 0;
+  }
+
+  open() {
+    this.opened = true;
+  }
+
+  start() {
+    this.question++;
+  }
+
+  onSubmit(value, answers): boolean {
+    for (const a in answers) {
+      if (value.toUpperCase() === answers[a].toUpperCase()) {
+        this.question++;
+        this.error = false;
+        if (this.question > 10) {this.finished = true; }
+        return true;
+      }
+    }
+    this.errorMessage = value + ' er ikke riktig!';
+    this.error = true;
+    return false;
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatFormFieldModule, MatInputModule, MatButtonModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
